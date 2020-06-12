6ff567bf413813c107e173afac8b97d1504e201b
Manuel Piroch <manuel@pepit.net>
2020-06-12 18:13:12 +0200

    refactored switch

--- NICE-P16F8x/Data.cs
+++ NICE-P16F8x/Data.cs
@@ -525,33 +525,32 @@ namespace NICE_P16F8x
         /// <returns></returns>
         public static int hexLookup(char c)
         {
-            switch (c)
+            return c switch
             {
-                case '0': return 0;
-                case '1': return 1;
-                case '2': return 2;
-                case '3': return 3;
-                case '4': return 4;
-                case '5': return 5;
-                case '6': return 6;
-                case '7': return 7;
-                case '8': return 8;
-                case '9': return 9;
-                case 'a': return 10;
-                case 'b': return 11;
-                case 'c': return 12;
-                case 'd': return 13;
-                case 'e': return 14;
-                case 'f': return 15;
-
-                case 'A': return 10;
-                case 'B': return 11;
-                case 'C': return 12;
-                case 'D': return 13;
-                case 'E': return 14;
-                case 'F': return 15;
-                default: return -1;
-            }
+                '0' => 0,
+                '1' => 1,
+                '2' => 2,
+                '3' => 3,
+                '4' => 4,
+                '5' => 5,
+                '6' => 6,
+                '7' => 7,
+                '8' => 8,
+                '9' => 9,
+                'a' => 10,
+                'b' => 11,
+                'c' => 12,
+                'd' => 13,
+                'e' => 14,
+                'f' => 15,
+                'A' => 10,
+                'B' => 11,
+                'C' => 12,
+                'D' => 13,
+                'E' => 14,
+                'F' => 15,
+                _ => -1,
+            };
         }
         #endregion
     }
